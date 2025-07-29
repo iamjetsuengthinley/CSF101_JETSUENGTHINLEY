e798e9e01ec34b26e5798ca0ddc19d21d1b7338e
iamjetsuengthinley <iamjetsuenthinley@gmail.com>
2025-07-29 14:35:09 +0600

    age

--- first_program.py
+++ first_program.py
@@ -1,5 +1,5 @@
-print("Kuzu, I am Jetsuen G Thinely")
-a=2
-b=3
-sum=a+b
-print(sum)
+print("Let me write you an intro.")
+name=input("What is your name?\n")
+age=input("How old are you?\n")
+
+print(f"Kuzuzangpo I am {name}, a {age}year old")
